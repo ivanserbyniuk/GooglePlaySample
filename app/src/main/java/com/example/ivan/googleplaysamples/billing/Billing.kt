af12f7e4bcebf1bd5d651ffe1ec77ad71f071778
ivanserbyniuk <ivan.serbyniuk@livestream.com>
2018-01-18 15:39:39 +0200

    Add Billing  class

--- app/src/main/java/com/example/ivan/googleplaysamples/billing/Billing.kt
+++ app/src/main/java/com/example/ivan/googleplaysamples/billing/Billing.kt
@@ -1,10 +1,12 @@
 package com.example.ivan.googleplaysamples.billing
 
 import android.content.Context
-import com.android.billingclient.api.BillingClient
-import com.android.billingclient.api.BillingClientStateListener
-import com.android.billingclient.api.Purchase
-import com.android.billingclient.api.PurchasesUpdatedListener
+import com.android.billingclient.api.*
+import com.android.billingclient.api.BillingClient.BillingResponse
+import com.android.billingclient.api.BillingClient.SkuType
+
+
+
 
 /**
  * Created by ivan on 1/18/18.
@@ -25,9 +27,29 @@ class Billing(context: Context) : PurchasesUpdatedListener {
             }
 
     override fun onPurchasesUpdated(responseCode: Int, purchases: MutableList<Purchase>?) {
-
+        if (responseCode == BillingResponse.OK && purchases != null) {
+            for (purchase in purchases) {
+                //handlePurchase(purchase)
+            }
+        } else if (responseCode == BillingResponse.USER_CANCELED) {
+            // Handle an error caused by a user cancelling the purchase flow.
+        } else {
+            // Handle any other error codes.
+        }
     }
 
+
+    fun query() {
+        val skuList = listOf ("premium_upgrade","gas" )
+        val params = SkuDetailsParams.newBuilder()
+        params.setSkusList(skuList).setType(SkuType.INAPP)
+        billingClient.querySkuDetailsAsync(params.build(), ::onQuerySuccess)
+    }
+
+    private fun onQuerySuccess(responseCode: Int, skuDetailsList: List<SkuDetails>) {
+
+
+    }
 
 
 }
